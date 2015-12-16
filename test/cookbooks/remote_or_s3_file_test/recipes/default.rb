ca0a0a313e375613dcbfd8430a74aae68d96f7ee
Henry Muru Paenga <meringu@gmail.com>
2015-12-17 11:35:39 +1300

    pass in credentials for testing outside of ec2 instances

--- test/cookbooks/remote_or_s3_file_test/recipes/default.rb
+++ test/cookbooks/remote_or_s3_file_test/recipes/default.rb
@@ -4,4 +4,7 @@ end
 
 remote_or_s3_file '/var/install' do
   source 's3://aws-codedeploy-us-east-1/latest/install'
+  aws_access_key_id node['aws']['aws_access_key_id']
+  aws_secret_access_key node['aws']['aws_secret_access_key']
+  aws_session_token node['aws']['aws_session_token']
 end
