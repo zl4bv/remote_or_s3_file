609b384e6e906845837119b984b890b4ab4dd385
Henry Muru Paenga <meringu@gmail.com>
2015-12-16 16:01:10 +1300

    Tidy up default resource

--- resources/default.rb
+++ resources/default.rb
@@ -1,4 +1,5 @@
 actions :create, :create_if_missing, :delete, :nothing, :touch
+
 default_action :create
 
 attribute :path, kind_of: String, name_attribute: true
@@ -11,5 +12,3 @@ attribute :backup, kind_of: [Integer, FalseClass], default: 5
 attribute :aws_access_key_id, kind_of: String
 attribute :aws_secret_access_key, kind_of: String
 attribute :aws_session_token, kind_of: String
-
-attr_accessor :exist
