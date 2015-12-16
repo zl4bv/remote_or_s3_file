1f6bc4fa0f829dbe499de1677314bfcab9f707b5
Henry Muru Paenga <meringu@gmail.com>
2015-12-17 11:35:55 +1300

    0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'meringu@gmail.com'
 license          'All rights reserved'
 description      'Recource wrapper for remote_file and aws_s3_file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends          'aws'
