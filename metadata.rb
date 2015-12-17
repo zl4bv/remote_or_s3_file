063704d0ac052f77dbb2522528991f35db6050be
Henry Muru Paenga <meringu@gmail.com>
2015-12-17 15:11:21 +1300

    urls for the supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'meringu@gmail.com'
 license          'All rights reserved'
 description      'Recource wrapper for remote_file and aws_s3_file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends          'aws'
+
+source_url 'https://github.com/meringu/remote_or_s3_file' if respond_to?(:source_url)
+issues_url 'https://github.com/meringu/remote_or_s3_file/issues' if respond_to?(:issues_url)
