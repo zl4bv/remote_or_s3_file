7bc593b99f8b70a6e23421430f3330cc1b61bfb6
sinkingpoint <colin@quirl.co.nz>
2016-04-11 13:49:12 +1200

    Added region attribute to provider/resource

--- resources/default.rb
+++ resources/default.rb
@@ -12,3 +12,4 @@ attribute :backup, kind_of: [Integer, FalseClass], default: 5
 attribute :aws_access_key_id, kind_of: String
 attribute :aws_secret_access_key, kind_of: String
 attribute :aws_session_token, kind_of: String
+attribute :region, kind_of: String
